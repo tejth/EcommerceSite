665f4d8915b565e698cc5cd4e4e70427d06b9e3c
tejth <ptejendra91@gmail.com>
2025-11-19 07:47:20 +0530

    Backend Setup

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import helmet from 'helmet'
+
+
+const app = express()
+app.use(cors({
+    credentials : true,
+    origin : process.env.FRONTEND_URL
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan())
+app.use(helmet({
+    crossOriginResourcePolicy:false
+}))
+
+
+const PORT = 8080 || process.env.PORT
+
+app.get("/",(request,response)=>{
+    //server to client
+    response.json({
+        message: "Server is Running " + PORT
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is running",PORT)
+})
