529e1dd7194562c6a2bff757af0a763770c5592a
tejth <ptejendra91@gmail.com>
2025-11-20 08:24:00 +0530

    verification of user logic

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -70,3 +70,41 @@ export async function registerUserController(request , response){
         })
     }
 }
+
+
+
+export async function verifyEmailController(request,response){
+    try {
+        const {code} = request.body
+        
+        const user = await UserModel.findOne({_id : code})
+
+
+        if(!user){
+            return response.status(400).json({
+                message: "Invalid code",
+                error: true,
+                success: false
+            })
+        }
+
+        const updateUser = await UserModel.updateOne({_id : code},{
+            verify_email : true
+        })
+
+        return response.json({
+            message : "Verification of email done!",
+            success: true,
+            error:false
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : true
+        })
+    }
+}
+
+
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,9 +1,12 @@
 import {Router} from 'express'
-import { registerUserController } from '../controllers/user.controller.js'
+import { registerUserController, verifyEmailController } from '../controllers/user.controller.js'
 
 
 const userRouter = Router()
 
 userRouter.post('/register',registerUserController)
+userRouter.post('/verify-email', verifyEmailController)
+
+
 
 export default userRouter
